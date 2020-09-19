f262fafb7c9e424c1ac7bb60622fc2ae8235b49e
Ajay Shete <ajay.shete@wgblindia.com>
2020-09-19 18:49:10 +0530

    user role deleted

--- db/migrate/20200702160736_devise_create_users.rb
+++ db/migrate/20200702160736_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.integer :role, null: false, default: 0
+
 
       ## Recoverable
       t.string   :reset_password_token
@@ -39,7 +39,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    add_index :users, :role
+    # add_index :users, :role
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -125,7 +125,6 @@ ActiveRecord::Schema.define(version: 2020_09_09_102744) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.integer "role", default: 0, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -133,7 +132,6 @@ ActiveRecord::Schema.define(version: 2020_09_09_102744) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["role"], name: "index_users_on_role"
   end
 
   add_foreign_key "areas", "cities"
